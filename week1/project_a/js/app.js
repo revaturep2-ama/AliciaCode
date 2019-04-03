26a4cb10ac99bd4f9d530023d05cb00316673ecb
alicialeblanc <alicialeblanc0.gmail.com>
2019-04-03 09:00:43 -0500

    backing up just in case

--- week1/project_a/js/app.js
+++ week1/project_a/js/app.js
@@ -1,3 +1,42 @@
 import {Location} from "./location.js";
 import {User} from "./user.js";
 
+function validateOrder()
+{
+    var sizeElem = document.getElementById('input [name = "size"] input[checked = true]');
+    var topElem = document.getElementById('input [name = "toppings"] input[checked = true]');
+
+    /*let size = document.querySelector('input[name="size"] input[checked = true]');
+    let toppings = document.querySelectorAll('input[name="toppings"] input[checked = true]');*/
+ 
+
+    if (!sizeElem)
+    {
+       console.log(('no size selected'));
+       return;
+    }
+
+    if (topElem.length == 0)
+    {
+        console.log(('no toppings selected'));
+        return;
+    }
+
+    
+        console.log(("order complete"));
+     
+
+}
+    
+
+    
+
+
+
+let placeOrder = document.querySelector('#placeOrder');
+
+if (placeOrder)
+{
+    placeOrder.addEventListener("click", validateOrder());
+}
+
