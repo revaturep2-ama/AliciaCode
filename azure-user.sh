000bacb74544d68131573184849e9df65b58ed67
alicialeblanc <alicia.leblanc0@gmail.com>
2019-04-21 05:08:00 +0000

    midnight backup

---
+++ azure-user.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+username=$1
+passcode=$2
+userdisplayname=$3
+userprincipalname=[$userdisplayname]$DOMAIN
+DOMAIN=$'@'alicialeblanc0gmail.onmicrosoft.com
+random=$(date +%-M)
+usersubscription=$4
+
+az login -u $username -p $passcode
+
+az ad user create --display-name $userdisplayname --password $random --user-principal-name $userprincipalname --force-change-password-next-login --subscription $usersubscription
+
+echo 'az-login complete'
+exit 0
+
+
