76887d43022c7499b234468a81a51d55b128f4a0
alicialeblanc <alicia.leblanc0@gmail.com>
2019-04-24 21:09:46 +0000

    fleshing out create funciton

--- scripts/azure-vm.sh
+++ scripts/azure-vm.sh
@@ -7,12 +7,28 @@ os=$4
 
 ##check if vm already exists
 
-if [
+if [ vm_name exists ]; then
+echo "this vm already exists"
+exit 1
+fi
+
+##check if rg exists
+
+if ! [ vm_rg exists ]; then
+echo "this resource group doesnt exist"
+exit 1
+fi
 
 ##funcitons to create and delete
 
 ##create function
+create(){
+if [ vm_rg exists ] && ! [ vm_name exists ]; then
+echo "creating vm"
+az vm create -n 
+fi
 
+}
 ##delete funciton
 
 ##casing
