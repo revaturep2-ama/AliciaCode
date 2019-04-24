db951aff7e7f20bba5a05656412b40086105eec1
alicialeblanc <alicia.leblanc0@gmail.com>
2019-04-24 20:37:40 +0000

    first azure terminal push

---
+++ scripts/azure-vm.sh
@@ -0,0 +1,18 @@
+##arguments for name, admin,  resource group and image
+
+vm_name=$1
+newadmin=$2
+vm_rg=$3
+os=$4
+
+##check if vm already exists
+
+if [
+
+##funcitons to create and delete
+
+##create function
+
+##delete funciton
+
+##casing
