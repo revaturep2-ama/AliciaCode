d1278d3e39bf3e634f06abce941a677a7dcee97d
alicialeblanc <alicia.leblanc0@gmail.com>
2019-04-25 14:18:29 +0000

    morning backup

--- scripts/azure-vm.sh
+++ scripts/azure-vm.sh
@@ -4,6 +4,7 @@ vm_name=$1
 newadmin=$2
 vm_rg=$3
 os=$4
+size=$5
 
 ##check if vm already exists
 
@@ -25,10 +26,22 @@ fi
 create(){
 if [ vm_rg exists ] && ! [ vm_name exists ]; then
 echo "creating vm"
-az vm create -n 
+az vm create -n $vm_name -g $vm_rg --image $os --admin-username $newadmin --size $size --generate-ssh-keys
 fi
 
 }
 ##delete funciton
+
+delete(){
+
+##check if vm exists
+if ! [ $(az vm list) | grep -E \$vm_name\ ]; then
+echo "this vm does not exist"
+exit 1
+fi
+
+
+
+}
 
 ##casing
