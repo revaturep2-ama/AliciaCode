64006371323fef35b612c07e362fe5e1df90a8b1
alicialeblanc <alicia.leblanc0@gmail.com>
2019-04-25 18:19:05 +0000

    new script: node-script.sh (subject to renaming)

---
+++ scripts/node-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+
+
+
+
+##calls script 2 of project 0
+curl 'https://raw.githubusercontent.com/1903-mar25-cloud/alicia-code/master/git-node-project.sh' | sh bash | xargs ~ project alicialeblanc alicia.leblanc0@gmail.com 
+
