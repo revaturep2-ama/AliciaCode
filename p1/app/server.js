f6d28e7fde6f23cb443d5098b736f41f37e88d3f
alicialeblanc <alicia.leblanc0@gmail.com>
2019-05-02 03:57:49 +0000

    server.js slight update

--- p1/app/server.js
+++ p1/app/server.js
@@ -5,8 +5,8 @@ const port = 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello Cloud Admin<h1>');
 });
 
 server.listen(port, hostname, () => {
