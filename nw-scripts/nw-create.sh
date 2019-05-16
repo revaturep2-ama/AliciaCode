fb3ed3887ac9fed9f44b6b6e235573a21e42f412
alicialeblanc <alicialeblanc0.gmail.com>
2019-05-16 12:07:12 -0500

    nw-create fleshed out

--- nw-scripts/nw-create.sh
+++ nw-scripts/nw-create.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
 ##create network
+nw=$1
+rg=$2
+ap=$3
+sn=$4
+sp=$5
 
+if ! [ $(az group exists -n $rg) ]; then
+echo "this resource group doesnt exist"
+exit 1
+fi
+
+if [ $(grep /($nw)/) | $(az network vnet list -g $rg) ]; then
+echo "this network already exists"
+exit 1
+fi
+
+if [ $(grep /($sn)) | $(az network vnet subnet list -g $rg --vnet-name $nw) ]; then
+echo "this subnet already exists in this network"
+exit 1
+fi
+
+az network vnet create -n $nw -g $rg --address-prefix $ap --subnet-name $sn --subnet-prefix $sp 
