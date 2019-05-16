4e01d4726fdcee09e70df9d7264d63e9779fd955
alicialeblanc <alicialeblanc0.gmail.com>
2019-05-16 11:07:30 -0500

    changed acl to acl-create, made acl-delete

---
+++ acl-delete.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+##nsg delete
+
+sgName=$1
+rg=$2
+
+if ! [ $(az group exists -n $rg) ]; then 
+
+echo "this resource group doesnt exist!"
+
+fi
+
+if ! [ $(grep \-w $sgName\ ) | $(az network nsg list) ]; then
+echo "this security group doesnt exist"
+fi
+
+az network nsg delete -n $sgName -g $rg
