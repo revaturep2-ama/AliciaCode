e565964ed427ff6a8931cb5de535f268caafca3e
alicialeblanc <alicialeblanc0.gmail.com>
2019-05-20 15:30:27 -0500

    test ad user w/o subscription option

---
+++ ad-user-create.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+##creating a new user
+
+userdisplayname=$1
+DOMAIN=$'@alicialeblanc0gmail.onmicrosoft.com'
+userprincipalname=$userdisplayname$DOMAIN
+pass=$2
+
+
+  az ad user create --user-display-name $userdisplayname --password $random --user-principal-name $userprincipalname --force-change-password-next-login
+
