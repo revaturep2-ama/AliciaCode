741bd94a1a3fd7250529c4aa30effda750300825
alicialeblanc <alicialeblanc0.gmail.com>
2019-05-24 09:22:25 -0500

    removed the rest of the unnecessary folders

--- ad-user-create.sh
+++ ad-user-create.sh
@@ -7,6 +7,5 @@ DOMAIN=$'@alicialeblanc0gmail.onmicrosoft.com'
 userprincipalname=$userdisplayname$DOMAIN
 pass=$2
 
-
-  az ad user create --user-display-name $userdisplayname --password $random --user-principal-name $userprincipalname --force-change-password-next-login
+  az ad user create --user-display-name $userdisplayname --password $pass --user-principal-name $userprincipalname --force-change-password-next-login
 
--- p1/create-app.sh
+++
@@ -1,6 +0,0 @@
-rg=$1
-appName=$2
-sku=S1
-sub=$3
-
-az appservice plan create -g $rg -n $appName --sku $sku --is-linux --subscription $sub
