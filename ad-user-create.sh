37d1b359b7d4dd17aa9b8a6d9668baa579808076
alicialeblanc <alicialeblanc0.gmail.com>
2019-05-27 20:44:25 -0500

    added validation

--- ad-user-create.sh
+++ ad-user-create.sh
@@ -7,6 +7,15 @@ DOMAIN=$'@mywebapp.southcentralus.cloudapp.azure.com'
 userprincipalname=$userdisplayname$DOMAIN
 pass=$2
 
+user=$(az ad user list --query [].userPrincipalName | grep $userprincipalname)
+     
+     #conditional to see if user exist
+
+     if [ -n $user ]; then
+     echo "user already exists"
+     exit 1
+    fi
+
   az ad user create --user-display-name $userdisplayname --password $pass --user-principal-name $userprincipalname --force-change-password-next-login
 
 az role assignment create --role reader --assignee $userprincipalname
